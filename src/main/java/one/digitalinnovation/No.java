9d1e0a279c9061fa31a5ac6f840e27cd2dd0790b
João Carlos Vernetti dos Santos <jcvernetti@gmail.com>
2022-02-24 23:01:42 -0300

    First commit - Pilha

---
+++ src/main/java/one/digitalinnovation/No.java
@@ -0,0 +1,34 @@
+package one.digitalinnovation;
+
+public class No {
+
+    private int dado;
+    private No refNo = null;
+
+    public No(int dado) {
+        this.dado = dado;
+    }
+
+    public int getDado() {
+        return dado;
+    }
+
+    public void setDado(int dado) {
+        this.dado = dado;
+    }
+
+    public No getRefNo() {
+        return refNo;
+    }
+
+    public void setReNo(No reNo) {
+        this.refNo = reNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "dado=" + dado +
+                '}';
+    }
+}
